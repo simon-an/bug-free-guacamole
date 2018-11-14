0f11348fa92b0c66c1e781beb55988c5fa7b50cf
simon-an <nomisimon@posteo.de>
2018-11-14 15:36:41 +0100

    exercise 4

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderWithSidenavComponent } from './components/header-with-sidenav/header-with-sidenav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  exports: [CommonModule]
+  declarations: [HeaderWithSidenavComponent],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [CommonModule, HeaderWithSidenavComponent]
 })
 export class SharedModule {}
--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from './../../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     SharedModule,
+    MatListModule,
     HomeRoutingModule
   ]
 })
